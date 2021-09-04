09a47cd83e265dc728216726c1edec5470e362c1
gabe martin <gmartin@archermalmo.com>
2021-09-03 19:27:47 -0500

    add new build. removing quotes from token.

--- src/components/Task.js
+++ src/components/Task.js
@@ -10,12 +10,12 @@ export default function Task({ task: { id, title, state }, onArchiveTask, onPinT
           defaultChecked={state === 'TASK_ARCHIVED'}
           disabled={true}
           name="checked"
-          style={{ background: 'red' }}
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
       <div className="title">
-        <input type="text" value={title} readOnly={true} placeholder="Input title" />
+        <input type="text" value={title} readOnly={true} placeholder="Input title"
+          style={{ background: '#cc0000' }}/>
       </div>
 
       <div className="actions" onClick={event => event.stopPropagation()}>
